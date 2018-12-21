08dc70f4772aaf5ada086d1967024bc6b58bdaa8
smallbee <smallbee3@gmail.com>
2018-12-21 12:32:03 +0900

    Card Retrieve, Update, Delete API added

--- app/cards/urls.py
+++ app/cards/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .apis import CardListCreateView
+from .apis import CardListCreateView, CardRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('', CardListCreateView.as_view()),
+    path('<int:pk>/', CardRetrieveUpdateDestroyView.as_view()),
 ]
