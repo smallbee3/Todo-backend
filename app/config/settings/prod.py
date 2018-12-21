3363219fc4a33beb89b5bd554578a8291117936d
smallbee <smallbee3@gmail.com>
2018-12-21 14:41:55 +0900

    ALLOWED_HOSTS updated

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -1,7 +1,12 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '.elasticbeanstalk.com',
+    '.amazonaws.com',
+]
+
 WSGI_APPLICATION = 'config.wsgi.prod.application'
 
 
