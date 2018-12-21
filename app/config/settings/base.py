515ee763593ab32522b6ac36bb8f619de17a486e
smallbee <smallbee3@gmail.com>
2018-12-21 11:51:49 +0900

    django-rest-framework installed

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party app
+    'rest_framework',
+
     # Custom app
     'users',
     'cards',
