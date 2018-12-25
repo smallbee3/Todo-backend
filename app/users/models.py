4a1b2f882b48db6fcf9f07ae74cba4000a8742fe
smallbee <smallbee3@gmail.com>
2018-12-25 22:26:54 +0900

    UserManager defined for making a logic of 'create_user'

--- app/users/models.py
+++ app/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
 from django.db import models
 
 __all___ = (
@@ -6,9 +6,21 @@ __all___ = (
 )
 
 
+class UserManager(DjangoUserManager):
+    def create_django_user(self, username, password, **extra_fields):
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            **extra_fields,
+        )
+        return user
+
+
 class User(AbstractUser):
     username = models.EmailField(unique=True)
     nickname = models.CharField(max_length=50, blank=True)
+
+    objects = UserManager()
 
     def __str__(self):
         return f'{self.pk} {self.username}'
